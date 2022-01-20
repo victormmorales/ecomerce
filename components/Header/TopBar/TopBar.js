d512a8b4e4d59a1e89a42056823d77519b26d603
victormmorales <victorfasrev@gmail.com>
2022-01-20 11:05:02 +0100

    navbar

---
+++ components/Header/TopBar/TopBar.js
@@ -0,0 +1,30 @@
+import { Container, Grid, Image, Input } from "semantic-ui-react";
+import Link from "next/link";
+
+export default function TopBar() {
+  return (
+    <div className="top-bar">
+      <Container>
+        <Grid className="top-bar">
+          <Grid.Column width={8} className="top-bar__left">
+            <Logo />
+          </Grid.Column>
+
+          <Grid.Column width={8} className="top-right">
+            <h2>Buscador</h2>
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <Link href="/">
+      <a>
+        <Image src="/logo.png" alt="Gaming" />
+      </a>
+    </Link>
+  );
+}
