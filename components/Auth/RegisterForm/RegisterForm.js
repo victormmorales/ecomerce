9e0e3c502246a7c83387ab7612df1f1561a12656
victormmorales <victorfasrev@gmail.com>
2022-01-21 03:32:38 +0100

    Register styles

--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+
 export default function RegisterForm(props) {
   const { showLoginForm } = props;
   return (
-    <div>
-      <h1>Registro formulario</h1>
-      <button onClick={showLoginForm}>Login</button>
-    </div>
+    <Form className="login-form">
+      <Form.Input name="name" type="text" placeholder="Nombre" />
+      <Form.Input name="lastname" type="text" placeholder="Apellido" />
+      <Form.Input name="username" type="text" placeholder="Nombre de usuario" />
+      <Form.Input name="email" type="text" placeholder="Email" />
+      <Form.Input name="password" type="password" placeholder="Contraseña" />
+
+      <div className="actions">
+        <Button type="button" basic>
+          Iniciar sesión
+        </Button>
+        <Button type="submit" className="submit">
+          Registrar
+        </Button>
+      </div>
+    </Form>
   );
 }
