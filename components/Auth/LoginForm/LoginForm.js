d72e8454b37fe356676c67da39831106be76477c
victormmorales <victorfasrev@gmail.com>
2022-01-21 13:20:14 +0100

    formik y yup login

--- components/Auth/LoginForm/LoginForm.js
+++ components/Auth/LoginForm/LoginForm.js
@@ -1,10 +1,62 @@
+import { useState } from "react";
+import { Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { toast } from "react-toastify";
+
 export default function LoginForm(props) {
   const { showRegisterForm } = props;
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <div>
-      <h1>Formulario de Login</h1>
-      <button onClick={showRegisterForm}>ir al registro</button>
-    </div>
+    <Form className="login-form" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="identifier"
+        type="text"
+        placeholder="Email"
+        onChange={formik.handleChange}
+        error={formik.error.identifier}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Contraseña"
+        onChange={formik.handleChange}
+        error={formik.error.password}
+      />
+
+      <div className="actions">
+        <Button type="button" basic onClick={showRegisterForm}>
+          Crear Cuenta
+        </Button>
+        <div>
+          <Button className="submit" type="submit">
+            Entrar
+          </Button>
+          <Button type="button">¿Has olvidado la contraseña?</Button>
+        </div>
+      </div>
+    </Form>
   );
+}
+
+function initialValues() {
+  return {
+    identifier: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    identifier: Yup.string().email(true).required(true),
+    password: Yup.string().required(true),
+  };
 }
--- components/Auth/RegisterForm/RegisterForm.js
+++ components/Auth/RegisterForm/RegisterForm.js
@@ -65,7 +65,7 @@ export default function RegisterForm(props) {
       />
 
       <div className="actions">
-        <Button type="button" basic>
+        <Button type="button" basic onClick={showLoginForm}>
           Iniciar sesión
         </Button>
         <Button type="submit" className="submit" loading={loading}>
